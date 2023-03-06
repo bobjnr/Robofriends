4ae4d5c6b1fe6dd10216c4dbcf3c4f6be25cdc49
bobjnr <bobekene7@gmail.com>
2023-03-06 15:31:15 +0000

    Updates

---
+++ precache-manifest.7f485c50d5b744598c99bcf28432a3f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e450a168db702895f62b",
+    "url": "/robofriends/static/css/main.253495e9.chunk.css"
+  },
+  {
+    "revision": "e450a168db702895f62b",
+    "url": "/robofriends/static/js/main.e450a168.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/robofriends/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "abbba7af437a5fd8efd3",
+    "url": "/robofriends/static/css/2.2df2161b.chunk.css"
+  },
+  {
+    "revision": "abbba7af437a5fd8efd3",
+    "url": "/robofriends/static/js/2.abbba7af.chunk.js"
+  },
+  {
+    "revision": "0e09ba9c7502e90c21a0",
+    "url": "/robofriends/static/js/runtime~main.0e09ba9c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "c2d173df6a190597dac54c309515a891",
+    "url": "/robofriends/index.html"
+  }
+];
