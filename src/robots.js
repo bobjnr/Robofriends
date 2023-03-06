d4e97f24758cb0a5a89431254549e92f2d509ef9
bobjnr <bobekene7@gmail.com>
2023-03-06 14:07:04 +0000

    adding project files

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Leanne Graham',
+	username: 'Bret',
+	email: 'Sincere@april.biz'	
+},
+{
+	id: 2,
+	name: 'Erwin Howell',
+	username: 'Antonette',
+	email: 'Shanna@melissa.tv'
+},
+{
+	id: 3,
+	name: 'Clementine Bauch',
+	username: 'Samantha',
+	email: 'Nathan@yesenia.net'
+},
+{
+	id: 4,
+	name: 'Patricia Lebsack',
+	username: 'Karianne',
+	email: 'Juliane.0Conner@kory.org'
+},
+{
+	id: 5,
+	name: 'Chelsea Dietrich',
+	username: 'Kamren',
+	email: 'Lucio_Hettinger@annie.ca'
+},
+{
+	id: 6,
+	name: 'Mrs. Dennis Schulist',
+	username: 'Leopoldpo_Corkey',
+	email: 'Karley_Dach@jasper.info'
+},
+{
+	id: 7,
+	name: 'Kurtis Weissnat',
+	username: 'Elwyn.Skiles',
+	email: 'Telly.Hoeger@billy.biz'
+},
+{
+	id: 8,
+	name: 'Nicholas Runolfsdottir V',
+	username: 'Maxine_Nienow',
+	email: 'Sherwood@rosamond.me'
+},
+{
+	id: 9,
+	name: 'Glenna Reichart',
+	username: 'Delphine',
+	email: 'Chain_McDermott@dana.io'
+},
+{
+	id: 10,
+	name: 'Clementina Dubuque',
+	username: 'Clement',
+	email: 'Rey.Padberg@karina.biz'
+}
+]
